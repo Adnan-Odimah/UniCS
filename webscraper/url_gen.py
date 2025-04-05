bc492d921d2ea0b5c9f79cbd918a74cfd79bd495
Adnan <Adnan11odimah@gmail.com>
2025-04-05 22:00:27 +0100

    added

--- webscraper/url_gen.py
+++ webscraper/url_gen.py
@@ -4,6 +4,7 @@ import json
 from collections import Counter
 from _func_aysnc import scrape_page
 import time
+from more_itertools import minmax
 
 BASE_URL = "https://scrapemequickly.com/all_cars?"
 RUN_ID = f"scraping_run_id="
@@ -21,15 +22,15 @@ async def get_token(scraping_run_id: str):
 
 async def start_scraping_run():
     async with aiohttp.ClientSession() as session:
-        #start = time.perf_counter()
+        start = time.perf_counter()
         async with session.post(f"https://api.scrapemequickly.com/scraping-run?team_id={TEAM_ID}") as response:
             if response.status != 200:
                 raise Exception("Failed to start scraping run")
-            return (await response.json())["data"]["scraping_run_id"]#, start
+            return (await response.json())["data"]["scraping_run_id"], start
 
 async def submit(answers: dict, scraping_run_id: str):
     max_retries = 5
-    base_delay = 0.0089
+    base_delay = 0.00888
 
     for attempt in range(max_retries):
         try:
@@ -71,9 +72,8 @@ def handle_data(data: dict):
             prices.append(el2["price"])
             makes.append(el2["make"])
 
-    min_year = min(years)
-    max_year = max(years)
-    avg_price = sum(prices) // len(prices)
+    min_year, max_year = minmax(years)
+    avg_price = sum(prices) / len(prices)
     mode_make = Counter(makes).most_common(1)[0][0]
 
     answers = {
@@ -82,6 +82,7 @@ def handle_data(data: dict):
         "avg_price": avg_price,
         "mode_make": mode_make
     }
+
     return answers
 
 async def worker(proxy, indices, scraping_run_id, token, BASE_DELAY, CONCURRENT_PER_PROXY):
@@ -89,39 +90,21 @@ async def worker(proxy, indices, scraping_run_id, token, BASE_DELAY, CONCURRENT_
     async with aiohttp.ClientSession() as session:
         tasks = [scrape_page(index, session, proxy, scraping_run_id, token, semaphore, BASE_DELAY) for index in indices]
         results = await asyncio.gather(*tasks, return_exceptions=True)
-        # Filter out errors and return only successful responses
-        for r in results:
-            if isinstance(r, Exception):
-                print(r)
         return [r for r in results if not isinstance(r, Exception)]
 
 async def main(CONCURRENT_PER_PROXY, BASE_DELAY):
     idx_split = END_IDX // len(PROXIES)
 
     starts_per = []
-    # each proxy needs to pic
-    # [1, 2, 3, 4, 5]
-    # 1 -> [0-24, 25-49, ..., idx_split]
-    # 2 -> [idx_split+1, idx_split+2, ..., idx_split*2]
-    # ...
-    # 5 -> [idx_split*4+1, idx_split*4+2, ..., END_IDX]
 
-    # code for it:
-    #print(idx_split)
     for i in range(5):
         starts_per.append([])
         for j in range(i*idx_split, (i+1)*idx_split, 25):
             starts_per[i].append(j)
-          #  print(j, i)
-          #  time.sleep(0.3)
-
-   # print(starts_per)
-
-   # return
 
 
-#    scraping_run_id, start = await start_scraping_run()
-    scraping_run_id = await start_scraping_run()
+    scraping_run_id, start = await start_scraping_run()
+#    scraping_run_id = await start_scraping_run()
     token = await get_token(scraping_run_id)
 
     tasks = [worker(PROXIES[i], starts_per[i], scraping_run_id, token, BASE_DELAY, CONCURRENT_PER_PROXY) for i in range(len(PROXIES))]
@@ -137,9 +120,9 @@ async def main(CONCURRENT_PER_PROXY, BASE_DELAY):
                 print(item)
 
     data = handle_data(flat_data)
-#    end = time.perf_counter()
-#    print(f"Time taken: {end - start} seconds")
-    await submit(data, scraping_run_id)
+    end = time.perf_counter()
+    print(f"Time taken: {end - start} seconds")
+    #await submit(data, scraping_run_id)
 
 
 
@@ -154,7 +137,7 @@ if __name__ == "__main__":
 
 #   1 |
 
-    CONCURRENT_PER_PROXY = 2
+    CONCURRENT_PER_PROXY = 3
     BASE_DELAY = 0.0097
 
     asyncio.run(main(CONCURRENT_PER_PROXY, BASE_DELAY))
